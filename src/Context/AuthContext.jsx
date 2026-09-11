f1fd0545e559e2dbfd0c2cd98118e1af42b62b5b
Uddhav Kamble <uddhavkamble356@gmail.com>
2026-09-11 17:57:32 +0530

    Change backend endpoint

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react'
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    const serverUrl = import.meta.env.VITE_SERVER_URL
+    const serverUrl = "http://ec2-13-60-63-20.eu-north-1.compute.amazonaws.com:8000"
 
     let [loading,setLoading]=useState(false)
 
