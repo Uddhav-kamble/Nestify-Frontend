4d6322189ab530c2cb70aba669bf5840755c02bf
Uddhav Kamble <uddhavkamble356@gmail.com>
2026-09-11 18:20:36 +0530

    Update titles

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -46,7 +46,7 @@ function SignUp() {
     <div className='w-[100vw] h-[100vh] flex items-center justify-center relative'>
         <div className='w-[50px] h-[50px] bg-[red] cursor-pointer absolute top-[10%] left-[20px] rounded-[50%] flex items-center justify-center' onClick={()=>navigate("/")}><FaArrowLeftLong className='w-[25px] h-[25px] text-[white]' /></div>
         <form action="" className='max-w-[900px] w-[90%] h-[600px] flex items-center justify-center flex-col md:items-start gap-[10px]' onSubmit={handleSignUP}>
-            <h1 className='text-[30px] text-[black]'>Welcome to Airbnb</h1>
+            <h1 className='text-[30px] text-[black]'>Welcome to Nestify</h1>
             <div className='w-[90%] flex items-start justify-start flex-col gap-[10px] mt-[30px] '>
           <label htmlFor="name" className='text-[20px]'>UserName</label>
           <input type="text" id='name' className='w-[90%] h-[40px] border-[2px] border-[#555656]  rounded-lg text-[18px] px-[20px] ' required onChange={(e)=>setName(e.target.value)} value={name}/>
