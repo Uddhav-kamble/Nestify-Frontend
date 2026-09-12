a413847ad649a2490004e2bed352c7f8123fba83
Uddhav Kamble <uddhavkamble356@gmail.com>
2026-09-12 19:02:41 +0530

    update serverurl

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState } from 'react'
 export const authDataContext = createContext()
 function AuthContext({children}) {
     const serverUrl = "http://ec2-13-60-63-20.eu-north-1.compute.amazonaws.com:8000"
+    // const serverUrl = "http://localhost:8000"
 
     let [loading,setLoading]=useState(false)
 
